26abe2e8a5b392b3dd5276ff83dc9e37c7acbc41
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-04-12 12:58:03 -0300

    Pokedex sem reatorização e sem estilização

--- src/pokedex/pokedex.tsx
+++ src/pokedex/pokedex.tsx
@@ -1,25 +1,71 @@
-import React, { useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-interface pokedexProps {
+interface pokedexProps {}
 
+interface PokemonListInterface {
+  name: string;
+  url: string;
 }
 
-export const pokedex: React.FC<pokedexProps> = () => {
+//const pokemonsArray: string[] = []
 
-    const [pokemons, setPokemons] = useState([]);
-    /* pokemons = Leitura dos dados   |   setPokemons = Modificação dos dados*/
-    const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>('Pikachu');
+function getDetailsfromPokemon(pokemon: PokemonListInterface) {}
 
+export const Pokedex: React.FC<pokedexProps> = () => {
+  const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+  /* pokemons = Leitura dos dados   |   setPokemons = Modificação dos dados*/
+  const [selectedPokemon, setSelectedPokemon] = useState<
+    PokemonListInterface | undefined
+  >(undefined);
+  const [selectedPokemonDetails, setSelectedPokemonDetails] = useState<
+    any | undefined
+  >(undefined);
 
-    return (
-        <div>
-            <h1>Pokedex</h1>
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => setPokemons(response.data.results)); // --> Aqui eu percorri dentro da api e depois determinei o array no qual eu queria que pegasse dentro da api, por isso tem o ""
+  }, []); //--> o array esta vazio pois eu quero que ele execurte essa função quando a aplicação for iniciada
 
+  useEffect(() => {
+    if (!selectedPokemon) return; // fiz com que se não tivesse pokemon selecionado, o useEffect nao vai continuar
 
+    alert(selectedPokemon.name);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`)
+      .then((response) => setSelectedPokemonDetails(response.data));
 
-            <h2>Pokemon selecionado: {selectedPokemon}</h2>
-        </div>
-    );
+    //  effect
+    //return () => {
+    //  cleanup
+    // }
+  }, [selectedPokemon]); //--> basicamente quando eu coloco algo aqui, eu digo que toda vez que a variavel 'selectedPokemon'(que é a que esta entre os colchetes) mudar, ele vai executar o useEffect novamente
+  return (
+    <div>
+      <h1>Pokedex</h1>
+
+      <h3>Pokemons:</h3>
+
+      {pokemons.map(
+        (pokemon) => (
+          <button onClick={() => setSelectedPokemon(pokemon)}>
+            {pokemon.name}
+          </button>
+        )
+
+        /*Fiz um map para mapear a lista pokemons, que mais acima foi definida no useState, e no onClick eu coloquei o 'setSelectedPokemon' que é o novo valor a ser colocado em 'SelectedPokemon'
+                e dentro do setSelectedPokemon eu coloquei o nome que eu dei para cada elemento dentro de 'pokemons' depois de mapear
+                */
+      )}
+
+      <h2>
+        Pokemon selecionado:{" "}
+        {selectedPokemon ? selectedPokemon.name : "Nenhum Pokemon Selecionado"}
+      </h2>
+      {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+    </div>
+  );
 };
 
-export default pokedex;
+export default Pokedex;
